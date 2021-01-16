8163ceadf1fe25cc2adcb9ddf1b9dfeccef57576
Stefan Marx <stefan@marx-merten.de>
2021-01-16 22:50:41 +0100

    enabled erase on full rebuild

--- pythonfirmware/fullRebuild.sh
+++ pythonfirmware/fullRebuild.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 rm -f .firmware_md5
 rm -f .life_md5
-./buildFirmware clean
+./buildFirmware.sh clean
+if [[ "$1" -eq "erase" ]]; then
+    ./buildFirmware.sh erase
+fi
 ./rebuild.sh $*
