5907d74eecb79fe2ba8b57d6e0eb8e698c8539bb
Stefan Marx <stefan@marx-merten.de>
2021-08-23 16:42:49 +0200

    sample scene

--- firmware/pysrc/live/scenes/basic.py
+++ firmware/pysrc/live/scenes/basic.py
@@ -4,17 +4,22 @@ class Rainbow(BaseScene):
     def __init__(self):
         super().__init__()
         self.pos=0
-        self.step=3
+        self.step=10
+        self.lauflicht_step=2
 
     def getName(self):
         return "rainbow"
 
+
     def frame(self, framecounter):
         leds=self.band.leds
-        count = len(leds)
-        for i in range(count):
-            hue=((i*self.step)+framecounter)%255
-            leds.set(i,hsv_plain=(hue,200,200))
+        if framecounter == 0 :
+            count = len(leds)
+            for i in range(count):
+                hue=(i*self.step)%255
+                leds.set(i,hsv_plain=(hue,200,255))
+        else:
+            leds >>=self.lauflicht_step
         return True
 
 def register(band:Band):
