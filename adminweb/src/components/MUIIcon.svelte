556315128d8258cb5e0d1a9e58da14bf83bbc0b9
Stefan Marx <stefan@marx-merten.de>
2021-08-29 23:18:40 +0200

    added XL mode for Icons

--- adminweb/src/components/MUIIcon.svelte
+++ adminweb/src/components/MUIIcon.svelte
@@ -1,13 +1,15 @@
 <script lang="ts">
   export let icon: string;
   let clazz: string = "";
-  export let size: "sm" | "md" | "lg" | "xl" = "md";
+  export let size: "sm" | "md" | "lg" | "xl" |"xxl"= "md";
 
   export { clazz as class };
   let sizeClazz = "md-24";
   $: {
-    sizeClazz = { sm: "md-18", md: "md-24", lg: "md-36", xl: "md-48" }[size];
+    sizeClazz = { sm: "md-18", md: "md-24", lg: "md-36", xl: "md-48",xxl:"md-128" }[size];
   }
+
+
 </script>
 
-<div class="material-icons {sizeClazz} {clazz}">{icon}</div>
+<div class="material-icons {sizeClazz} {clazz}"  >{icon}</div>
