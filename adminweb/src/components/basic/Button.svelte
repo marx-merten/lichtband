55a75dbd4c4f956d8a9c03bcaf9bcba6d91442cc
Stefan Marx <stefan@marx-merten.de>
2021-08-29 23:18:56 +0200

    SIMPLE Butons

---
+++ adminweb/src/components/basic/Button.svelte
@@ -0,0 +1,15 @@
+
+<script lang="ts">
+    import { createEventDispatcher } from "svelte";
+    const dispatch = createEventDispatcher();
+    export let checked=false
+    let checkedClazz=""
+    $:{
+      checkedClazz=checked?"bg-theme-secondary":"bg-theme-primary"
+    }
+  </script>
+
+<button type="button" class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md shadow-sm  hover:bg-theme-secondary    text-theme-onPrimary {checkedClazz} "
+on:click={(ev) => dispatch("click", ev.detail)}>
+    <slot/>
+</button>
