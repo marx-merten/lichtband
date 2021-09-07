8a64795d38ad03cc296e2d4313a4129d6e0dba2a
Stefan Marx <stefan@marx-merten.de>
2021-09-07 23:25:50 +0200

    allow for define ip address

--- firmware/remote-upload-webapp.py
+++ firmware/remote-upload-webapp.py
@@ -1,5 +1,8 @@
 import os
 import requests
+import sys
+
+
 def walk_dir(path,prefix=""):
     results=[]
     for f in os.listdir(path):
@@ -9,12 +12,17 @@ def walk_dir(path,prefix=""):
             results+=walk_dir(path+f+"/",prefix+f+"/")
     return results
 
+def uploadDir(host,basedir):
+    requests.delete(url="http://{}/api/fs/dir/html?recurse=true".format(host))
+    for f in walk_dir(basedir):
+        print(f)
+        url = "http://{}/api/fs/file/html/{}".format(host,f)
+        file = "{}{}".format(basedir,f)
+        print("uploaded {} to {}".format(file,url))
+        requests.put(url=url,data=open(file,"rb"))
 
-requests.delete(url="http://172.17.100.43/api/fs/dir/html?recurse=true")
-
-basedir="../adminweb/build/"
-for f in walk_dir(basedir):
-    url = "http://172.17.100.43/api/fs/file/html/{}".format(f)
-    file = "{}{}".format(basedir,f)
-    print("uploaded {} to {}".format(file,url))
-    requests.put(url=url,data=open(file,"rb"))
+if __name__ == "__main__":
+    if len(sys.argv)<3 :
+        print ("Usage: {} <hostname> <local-web-app>".format(sys.argv[0]))
+    else :
+        uploadDir(sys.argv[1],sys.argv[2])
