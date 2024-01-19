3aff5c0931a0b4ed4091845152e2388bb5e803ce
Stefan Marx <stefan@marx-merten.de>
2024-01-19 14:15:08 +0100

    update to 2024 micropython and idf 5_0_4

--- firmware/environment.sh
+++ firmware/environment.sh
@@ -2,13 +2,15 @@ curdir=$(pwd)
 echo ${curdir}
 echo =====================
 export MICROPYTHON_DIR=${curdir}/../../../environments/micropython-custom
-export ESPIDF=${curdir}/../../../environments/esp32/espidf4_4
+export ESPIDF=${curdir}/../../../environments/esp32/espidf5_0
 . ${ESPIDF}/export.sh
 
-export BOARD=GENERIC_OTA
-export BOARD_DIR=${curdir}/boards/CUSTOM
+export BOARD=ESP32_GENERIC
+export BOARD_VARIANT=OTA
+
+# export BOARD_DIR=${curdir}/boards/CUSTOM
 export FLASH_SIZE=4MB
-export PORT=$(ls -1 /dev/tty.usbserial-*|head -n1)
+export PORT=$(ls -1 /dev/tty.usbserial-* | head -n1)
 #export PORT=/dev/cu.SLAB_USBtoUART
 # export PORT=/dev/tty.usbserial-AD0KDXKW
 export FROZEN_MANIFEST=${curdir}/frozenmanifest.py
